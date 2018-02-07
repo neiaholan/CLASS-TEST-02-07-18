09a693da1aa1c032826196b432f4a443e6df1ffc
neiaholan <35816388+neiaholan@users.noreply.github.com>
2018-02-07 15:09:32 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,16 @@
-package test;
+public class PhoneTest {
 
-public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello World");
-	}
+	public static void main(String [] args);
+	
+	Phone phone1= new Phone("Samsung, Ice Cream, 1994, 199.99")
+			System.out.println(Phone1.PhoneBrand);
+	
+	Phone phone2= new Phone("Verizon, iPhone, 2000, 5000.00")
+			System.out.println(Phone2.PhoneBrand);
+	
+	Phone phone3= new Phone("T-Mobile, Android, 1999, 50.00")
+			System.out.println(Phone3.PhoneBrand);
+	
+	System.out.println("Which phone would you like to Purchase?")
 
 }
