33085fe5c305d12ac0ae8bedf2a14825afbec1cf
Hunter <NOHunter.2014@MyMail.jcsu.edu>
2018-02-07 14:53:39 -0500

    first commit

---
+++ Phone.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Phone {
+String Brand;
+String PhoneName;
+int YearReleased;
+double Price;
+
+public Phone (String Brand2, String PhoneName2, int YearReleased2, double Price2){
+	
+
+Brand = Brand2;
+PhoneName = PhoneName2;
+YearReleased = YearReleased2;
+Price = Price2;
+
+
+
+}}
